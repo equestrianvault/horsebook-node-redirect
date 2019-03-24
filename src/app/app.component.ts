b2b64a9d79c00fc4b144ec844c881b4a577ba2fe
Nathan Brown <nrb62281@gmail.com>
2019-03-24 00:25:52 -0700

    Fixed site to redirect.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'horsebook-node-redirect';
+export class AppComponent implements OnInit {
+  title = 'Crystal Archives';
+  constructor(){}
+  ngOnInit(){
+  	window.location.href = "https://www.equestrianvault.com";
+  }
 }
